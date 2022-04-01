791e0593f7cd8ac40d3ec2f06685babb2b6d6a9a
Amine913 <amine.bennjima@fitmentindustries.com>
2022-03-31 19:30:55 -0500

    Updated Master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Test");
-        // test GitHub push
-        // test 2 electic boogaloo
     }
 }
